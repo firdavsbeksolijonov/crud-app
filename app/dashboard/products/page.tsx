b7d603ab13fb6c773bbe83a43c727e05854904cd
Firdavs <abdullayevbotirjon579@email.com>
2024-06-20 22:17:10 +0500

    first

---
+++ app/dashboard/products/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Productspage = () => {
+  return (
+    <div>Products page</div>
+  )
+}
+
+export default Productspage
---
+++ app/types/layout.ts
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react';
+ export interface MenuItem {
+    title: string,
+    list: SubMenuItem[]
+}
+export interface SubMenuItem {
+    title: string,
+    path: string,
+    icon: ReactNode;
+
+}
